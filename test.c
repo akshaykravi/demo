b600a81288b7f885663699bf4f131475ce792bdd
akshaykravi <141711757+akshaykravi@users.noreply.github.com>
2023-08-09 12:47:30 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("------------------------------------\n");
+  printf("Hello, this is a test file for yocto\n");
+  printf("------------------------------------\n");
+
+  return 0;
+}
